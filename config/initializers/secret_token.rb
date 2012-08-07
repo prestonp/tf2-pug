a6dd54866d5bd5902834554a59137ca16ca6781a
Preston Pham <prestonp08@gmail.com>
2012-08-07 03:34:03 -0500

    Adding empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tf2Pug::Application.config.secret_token = '4d96dd30fe6dce09487ef80e5f7213e25f0b7f343b6ce7301ffd2d4bd45d105214ddc758b8ff430458e506ae455ede118274f693b064ae9ed461aad189c1e154'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tf2Pug::Application.config.session_store :cookie_store, key: '_tf2-pug_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tf2Pug::Application.config.session_store :active_record_store
